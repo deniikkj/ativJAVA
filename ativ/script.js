d20425bbb69bab5babf211e0bd62200772bcce83
deniikkj <lanemarcioliveira.10@gmail.com>
2024-06-28 19:17:47 -0300

    inicio 1-5

---
+++ ativ/script.js
@@ -0,0 +1,103 @@
+// // 1.
+
+// let a = parseInt(prompt('Digite o valor de A'))
+// let b = parseInt(prompt('Digite o valor de B'))
+// let c= parseInt(prompt('Digite o valor de C'))
+
+// let aMaisB = a + b 
+
+// if (aMaisB < c) {
+//     console.log('a + b é menor que c')
+// } else {
+//     console.log('a + b é maior que c')
+// }
+
+// // 2.
+
+// function lerDados() {
+
+//     let nome = prompt("Digite o nome da pessoa:");
+//     let sexo = prompt("Digite o sexo da pessoa (M ou F):").toUpperCase();
+//     let estadoCivil = prompt("Digite o estado civil da pessoa (SOLTEIRO, CASADO, DIVORCIADO, VIÚVO):").toUpperCase();
+
+//     if (sexo === "F" && estadoCivil === "CASADA") {
+ 
+//         let tempoCasada = parseInt(prompt("Digite o tempo de casamento em anos:"));
+
+//         console.log(A pessoa ${nome} é do sexo feminino, casada há ${tempoCasada} anos.);   
+        
+//     } else {
+        
+//         console.log(A pessoa ${nome} não se enquadra na condição de sexo feminino e/ou estado civil casada.);
+//     }
+// }
+
+
+// lerDados();
+
+// // 3.
+
+// function parOuImpar(numero) {
+
+//   if (numero % 2 === 0) {
+
+//     console.log(${numero} é par.)
+
+//   } else {
+    
+//     console.log(${numero} é ímpar.);
+//   }
+// }
+
+// let numero = parseInt(prompt("Digite um número"))
+
+// parOuImpar(numero);
+
+// // 4.
+
+// function resultado(a, b) {
+//     let c
+
+//     if (a === b) {
+        
+//         c = a + b
+
+//     } else {
+        
+//         c = a * b
+
+//     }
+
+//     console.log(O resultado é: ${c});
+// }
+//     let valorA = parseInt(prompt("Digite o valor de A:"))
+//     let valorB = parseInt(prompt("Digite o valor de B"))
+
+//     resultado(valorA, valorB)
+
+// // 5.
+
+// function dobroOuTriplo(numero) {
+    
+//     let resultado
+
+//     if (numero > 0) {
+
+//         resultado = numero * 2
+
+//         console.log(O dobro de ${numero} é ${resultado}.)
+
+//     } else if (numero < 0) {
+
+//         resultado = numero * 3
+        
+//         console.log(O triplo do ${numero} é ${resultado})
+
+//     } else {
+//         console.log("O número é zero, sendo assim, não há como multiplicar")
+//     }
+// }
+
+// let numero = parseInt(prompt("Digite um número"))
+
+// dobroOuTriplo(numero)
