dec4f43fb4477107b3a3d24d693f29afc5c6cec5
deniikkj <lanemarcioliveira.10@gmail.com>
2024-06-29 20:07:03 -0300

    quase pronto

--- ativ/script.js
+++ ativ/script.js
@@ -101,3 +101,159 @@
 // let numero = parseInt(prompt("Digite um número"))
 
 // dobroOuTriplo(numero)
+
+// // 6.
+
+// function verificarValoresBooleanos() {
+
+//     let valor1 = prompt("Digite o primeiro valor:");
+//     let valor2 = prompt("Digite o segundo valor:");
+
+//     valor1 = (valor1 === 'true');
+//     valor2 = (valor2 === 'true');
+
+//     if (valor1 && valor2) {
+//         alert("Ambos valores são verdeiros.");
+
+//     } else if (!valor1 && !valor2) {
+//         alert("Ambos valores são falsos.");
+
+//     } else {
+//         alert("Um dos valores é verdadeiro e o outro é falso");
+
+//     }
+// }
+
+// verificarValoresBooleanos();
+
+// // 7.
+
+// function somaCondicional(){
+
+//     let numero = parseInt(prompt("Digite um número:"))
+
+//     if (numero % 2 === 0) {
+//         let resultado = numero + 5
+
+//         alert(`O número ${numero} é par. Adicionando 5 da igual à ${resultado}.`)
+
+//     } else {
+//         let resultado = numero + 8
+
+//         alert(`O número ${numero} é impar. Adicionando 8 da igual à ${resultado}.`)
+
+//     }
+// }
+
+//     somaCondicional()
+
+// // 8.
+
+// function ordemDecrescente() {
+
+//     let numero1 = parseInt(prompt("Digite o primeiro número:"))
+
+//     let numero2 = parseInt(prompt("Digite o segundo número:"))
+
+//     let numero3 = parseInt(prompt("Digite o terceiro número:"))
+
+//     let primeiro, segundo, terceiro;
+
+//     if (numero1 > numero2 && numero1 > numero3) {
+//         primeiro = numero1
+        
+//         if (numero2 > numero3) {
+//             segundo = numero2
+//             terceiro = numero3
+
+//         } else {
+//             segundo = numero3
+//             terceiro = numero2
+//         }
+
+//     } else if (numero2 > numero1 && numero2 > numero3) {
+//         primeiro = numero2
+
+//         if (numero1 > numero3) {
+//             segundo = numero1
+//             terceiro = numero3
+
+//         } else {
+//             segundo = numero3
+//             terceiro = numero1
+//         }
+        
+//     } else {
+//         primeiro = numero3
+
+//         if (numero1 > numero2) {
+//             segundo = numero1
+//             terceiro = numero2
+
+//         } else {
+//             segundo = numero2
+//             terceiro = numero1
+//         }
+//     }
+
+//     alert(`Números em ordem decrescente: ${primeiro}, ${segundo}, ${terceiro}`)
+// }
+
+// ordemDecrescente()
+
+// // 9.
+
+// function calcularPeso() {
+
+//     let altura = parseFloat(prompt("Digite a altura (em metros):"))
+
+//     let sexo = prompt("Digite o sexo (M ou F):").toUpperCase()
+
+//     let pesoIdeal;
+
+//     if (sexo === 'M') {
+//         pesoIdeal = (72.7 * altura) - 58
+//         alert(`O peso ideal para um homem com altura ${altura.toFixed(2)} m é ${pesoIdeal.toFixed(2)} kg.`)
+
+//     } else if (sexo === 'F') {
+//         pesoIdeal = (62.1 * altura) - 44.7
+//         alert(`O peso ideal para uma mulher com altura ${altura.toFixed(2)} m é ${pesoIdeal.toFixed(2)} kg.`)
+
+//     } else {
+//         alert("Sexo inválido. Por favor, digite 'M' para masculino ou 'F' para feminino.")
+
+//     }
+// }
+
+// calcularPeso();
+
+// // 10. 
+
+// function calcularIMC() {
+
+//     let peso = parseFloat(prompt("Digite o peso (em kg):"))
+
+//     let altura = parseFloat(prompt("Digite a altura (em metros):"))
+
+//     let imc = peso / (altura * altura)
+
+//     let condicao
+//     if (imc < 18.5) {
+//         condicao = "Abaixo do peso."
+
+//     } else if (imc >= 18.5 && imc < 25) {
+//         condicao = "Peso normal."
+
+//     } else if (imc >= 25 && imc < 30) {
+//         condicao = "Acima do peso."
+
+//     } else {
+//         condicao = "Obeso"
+//     }
+
+//     alert(`IMC calculado: ${imc.toFixed(2)} - Condição: ${condicao}`)
+// }
+
+// calcularIMC();
+
+// 11.
